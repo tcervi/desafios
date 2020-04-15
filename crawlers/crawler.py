072b5f012cf3f1e18bd6626e2bb66f0738179796
tcervi <thecervi@gmail.com>
2020-04-15 15:39:42 -0300

    Creating logic to filter threads by a given 'data-score'

--- crawlers/crawler.py
+++ crawlers/crawler.py
@@ -22,20 +22,45 @@ class HotThreadResult:
 OLD_REDDIT_URL = "https://old.reddit.com/r/"
 
 
-try:
-    # html = urlopen(OLD_REDDIT_URL + "Art")
-    # res = BeautifulSoup(html.read(), "html5lib")
-    output_file = open("resSample.html", "r")
-    res = BeautifulSoup(output_file.read(), "html5lib")
-    output_file.close()
+def extract_hot_threads(threads_raw, min_score=5000):
+    """Given a PageElements set, with any number of threads page elements, and a minimum score to consider one thread
+    as a hot thread, returns a list containing only the hot threads from the original set
 
-    print(res.title)
-    threads_raw = res.findAll("div", {"id": re.compile("thing_t3_")})
-    threads_hot = []
-    print(len(threads_raw))
+    :param threads_raw: the raw PageElements set to be filtered
+    :param min_score: the minimum score to consider a thread valid
+    :return: a list containing only the hot threads
+    """
+
+    hot_threads = []
     for thread in threads_raw:
-        print(thread)
-except HTTPError as error:
-    print(error)
-except URLError as error:
-    print(error)
+        score = thread["data-score"]
+        if score is None or int(score) < min_score:
+            continue
+        else:
+            print(thread["data-score"])
+            hot_threads.append(thread)
+    else:
+        return hot_threads
+
+
+def main():
+    try:
+        # html = urlopen(OLD_REDDIT_URL + "Art")
+        # res = BeautifulSoup(html.read(), "html5lib")
+        output_file = open("resSample.html", "r")
+        res = BeautifulSoup(output_file.read(), "html5lib")
+        output_file.close()
+
+        print(res.title)
+        threads_raw = res.findAll("div", {"id": re.compile("thing_t3_")})
+        print(len(threads_raw))
+        threads_hot = extract_hot_threads(threads_raw)
+
+    except HTTPError as error:
+        print(error)
+    except URLError as error:
+        print(error)
+
+
+if __name__ == '__main__':
+    main()
